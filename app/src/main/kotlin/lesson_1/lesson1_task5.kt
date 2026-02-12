17ce5708a90990bbeb8aab7e48855da417f2c0f6
gosh1894 <gurgen94@outlook.com>
2026-02-12 08:53:20 +0300

    KS-1-5 task5

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,12 @@
 package lesson_1
 fun main() {
-    val totalSeconds = 108 * 60 + 18
+    val totalSeconds = 6480
 
     val hours = totalSeconds / 3600
-    val minutes = (totalSeconds % 3600) / 60
-    val seconds = totalSeconds % 60
-
-    // Самый Kotlin-способ форматирования
-    val timeString = "%02d:%02d:%02d".format(hours, minutes, seconds)
-    println("Время, проведенное в космосе: $timeString")
+    val remainingAfterHours = totalSeconds / 3600
+    val minutes = remainingAfterHours / 60
+    val seconds = remainingAfterHours % 60
+    println(hours)
+    println(minutes)
+    println(seconds)
 }
